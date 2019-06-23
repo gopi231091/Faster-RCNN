3e04c0994f31130ec0ca9a236cb38992c946c534
Gopi Krishna Erabati <gopi231091@gmail.com>
2019-06-23 21:28:22 +0100

    Update test_frcnn.py

--- test_frcnn.py
+++ test_frcnn.py
@@ -340,12 +340,12 @@ if ((options.isImage) and (not options.isMap)):
 				textLabel = '{} {}'.format(key,int(100*new_probs[jk]))
 				all_dets.append((key,100*new_probs[jk]))
 
-				(retval,baseLine) = cv2.getTextSize(textLabel,cv2.FONT_HERSHEY_COMPLEX,1,1)
+				(retval,baseLine) = cv2.getTextSize(textLabel,cv2.FONT_HERSHEY_PLAIN,1,1)
 				textOrg = (real_x1, real_y1-0)
 
 				cv2.rectangle(img, (textOrg[0] - 5, textOrg[1]+baseLine - 5), (textOrg[0]+retval[0] + 5, textOrg[1]-retval[1] - 5), (int(class_to_color[key][0]), int(class_to_color[key][1]), int(class_to_color[key][2])), 1)
 				cv2.rectangle(img, (textOrg[0] - 5,textOrg[1]+baseLine - 5), (textOrg[0]+retval[0] + 5, textOrg[1]-retval[1] - 5), (int(class_to_color[key][0]), int(class_to_color[key][1]), int(class_to_color[key][2])), -1)
-				cv2.putText(img, textLabel, textOrg, cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 0), 1)
+				cv2.putText(img, textLabel, textOrg, cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1)
 
 		print('Elapsed time = {}'.format(time.time() - st))
 		print(all_dets)
